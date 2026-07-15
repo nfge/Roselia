9baf679398289dba11c8c03d5344f1210b023e2f
nfge <165654547+nfge@users.noreply.github.com>
2026-07-15 18:57:58 +0300

    unstable

--- kernel/src/cpu/gdt.rs
+++ kernel/src/cpu/gdt.rs
@@ -1,5 +1,8 @@
 use lazy_static::lazy_static;
-use x86_64::structures::{gdt::{Descriptor, GlobalDescriptorTable}, tss::TaskStateSegment};
+use x86_64::{VirtAddr, registers::segmentation::{CS, SS, Segment}, structures::{
+    gdt::{Descriptor, GlobalDescriptorTable},
+    tss::TaskStateSegment,
+}};
 
 lazy_static! {
     static ref GDT: GlobalDescriptorTable = {
@@ -12,7 +15,13 @@ lazy_static! {
 }
 lazy_static! {
     static ref TSS: TaskStateSegment = {
-        let tss = TaskStateSegment::new();
+        let mut tss = TaskStateSegment::new();
+        tss.interrupt_stack_table[0] = {
+            const STACK_SIZE: usize = 4096 * 5;
+            static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
+            let stack_start = VirtAddr::from_ptr(&raw const STACK);
+            stack_start + STACK_SIZE as u64
+        };
         tss
     };
 }
