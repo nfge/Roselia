ff509f61ee577ed5600f9a5450f4b45c9776abbf
nfge <165654547+nfge@users.noreply.github.com>
2026-07-22 11:04:32 +0300

    not working

--- common/pci/src/lib.rs
+++ common/pci/src/lib.rs
@@ -6,7 +6,7 @@ extern crate alloc;
 use acpi_tables::mcfg::McfgEntry;
 
 use crate::headers::{PciDevice, PciHeader};
-use alloc::{format, vec::Vec};
+use alloc::{boxed::Box, format, vec::Vec};
 pub mod headers;
 
 pub const PCI_IDS: &[u8] = include_bytes!("pci.ids");
@@ -40,32 +40,48 @@ pub unsafe fn enumerate(entry: &McfgEntry) -> Vec<PciDevice> {
     devices
 }
 
-pub fn check(vendor_id: u16, device_id: u16) -> Option<(Option<&'static str>, Option<&'static str>)> {
-    let (mut vendor, mut device): (&str, &str);
-    let text = core::str::from_utf8(PCI_IDS).unwrap();
-    let mut lines = text.lines();
-    while let Some(line) = lines.next() {
-        if line.starts_with(format!("{:04x}", vendor_id).as_str()) {
-            vendor = &line[6..].trim();
-            while let Some(device_line) = lines.next() {
-                let trimmed = device_line.trim_start();
-                let indent = device_line.len() - trimmed.len();
+pub fn check(
+    vendor_id: u16,
+    device_id: u16,
+) -> Option<(Option<&'static str>, Option<&'static str>)> {
+    let text = core::str::from_utf8(PCI_IDS).ok()?;
 
-                if indent == 0 {
-                    break; 
-                }
-                let deeper = trimmed.trim_start();
-                if trimmed.len() != deeper.len() {
+    let vendor_id = format!("{:04x}", vendor_id);
+    let device_id = format!("{:04x}", device_id);
+
+    let mut vendor_name: Option<&'static str> = None;
+
+    for line in text.lines() {
+        if line.is_empty() || line.starts_with('#') {
+            continue;
+        }
+        if !line.starts_with('\t') && !line.starts_with(' ') {
+            let mut parts = line.split_whitespace();
+
+            if let Some(id) = parts.next() {
+                if id == vendor_id {
+                    let name = line[id.len()..].trim();
+                    vendor_name = Some(name);
                     continue;
                 }
-
-                if trimmed.starts_with(format!("{:04x}", device_id).as_str()) {
-                    device = trimmed[4..].trim();
-                    return Some((Some(vendor), Some(device)));
+                if vendor_name.is_some() {
+                    break;
                 }
             }
-            return Some((Some(vendor), None));
+            if vendor_name.is_some() {
+                let trimmed = line.trim_start();
+
+                let mut parts = trimmed.split_whitespace();
+
+                if let Some(id) = parts.next() {
+                    if id == device_id {
+                        let name = trimmed[id.len()..].trim();
+                        return Some((vendor_name, Some(name)));
+                    }
+                }
+            }
         }
     }
-    None
+
+    vendor_name.map(|v| (Some(v), None))
 }
