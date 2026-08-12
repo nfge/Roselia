3d53255b78e9edbf76c708f3d0ff925a7a3193db
nfge <165654547+nfge@users.noreply.github.com>
2026-08-12 15:51:20 +0300

    mini test

--- kernel/build.rs
+++ kernel/build.rs
@@ -6,4 +6,14 @@ fn main() {
     .output()
     .unwrap();
     println!("cargo:rustc-env=GIT_COMMIT={}", String::from_utf8_lossy(&hash.stdout).trim());
+
+    // cc::Build::new()
+    //     .file("./src/terminal/test.c")
+    //     .compiler("clang")
+    //     .flag("-ffreestanding")
+    //     .flag("-fno-stack-protector")
+    //     .target("x86_64-unknown-none")
+    //     .archiver("llvm-ar")
+    //     .compile("test");
+
 }
