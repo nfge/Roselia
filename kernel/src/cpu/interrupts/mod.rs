c8e6925a9732c705731fd9e3bb7fea00547fd174
nfge <165654547+nfge@users.noreply.github.com>
2026-08-13 22:26:10 +0300

    added breakpoint_handler

--- kernel/src/cpu/interrupts/mod.rs
+++ kernel/src/cpu/interrupts/mod.rs
@@ -1,17 +1,26 @@
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
-use crate::{keyboard, kprintln,timer};
+use crate::{keyboard, kprintln, log, timer};
 use utils::serial_println;
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
-        
-        unsafe {idt[32].set_handler_fn(timer::irq::timer_handler).set_stack_index(0)};
-        unsafe{idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(0)};
+
+        unsafe {
+            idt[32]
+                .set_handler_fn(timer::irq::timer_handler)
+                .set_stack_index(0)
+        };
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
+                .set_stack_index(0)
+        };
         idt.invalid_opcode.set_handler_fn(invalid_opcode_handler);
         idt.debug.set_handler_fn(debug_handler);
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
         idt.general_protection_fault.set_handler_fn(gp_handler);
         idt[33].set_handler_fn(keyboard::irq::keyboard_irq);
         idt[0xFF].set_handler_fn(spurious_handler);
@@ -31,14 +40,25 @@ extern "x86-interrupt" fn spurious_handler(_: InterruptStackFrame) {
 // }
 extern "x86-interrupt" fn double_fault_handler(_: InterruptStackFrame, _: u64) -> ! {
     serial_println!("Double fault");
-    loop {};
+    loop {}
 }
 extern "x86-interrupt" fn invalid_opcode_handler(stack: InterruptStackFrame) {
     panic!("Invalid opcode\n{:#?}", stack)
 }
-extern "x86-interrupt" fn gp_handler(_stack:InterruptStackFrame, code: u64) {
+extern "x86-interrupt" fn gp_handler(_stack: InterruptStackFrame, code: u64) {
     panic!("General Protection\n{:#?}", code);
 }
-extern "x86-interrupt" fn debug_handler(stack:InterruptStackFrame) {
+extern "x86-interrupt" fn debug_handler(stack: InterruptStackFrame) {
     serial_println!("[Debug] {:#?}", stack);
+}
+extern "x86-interrupt" fn breakpoint_handler(_stack: InterruptStackFrame) {
+    let secs = crate::timer::TICKS.load(core::sync::atomic::Ordering::Relaxed) / crate::timer::TICKS_PER_SEC.load(core::sync::atomic::Ordering::Relaxed);
+    if secs == 0 {
+        serial_println!("Reached breakpoint at {}ms", crate::timer::TICKS.load(core::sync::atomic::Ordering::Relaxed) * 1000 / crate::timer::TICKS_PER_SEC.load(core::sync::atomic::Ordering::Relaxed));
+        log!("Reached breakpoint at {}ms\n", crate::timer::TICKS.load(core::sync::atomic::Ordering::Relaxed) * 1000 / crate::timer::TICKS_PER_SEC.load(core::sync::atomic::Ordering::Relaxed));
+    } else {
+        serial_println!("Reached breakpoint at {}s", secs);
+        log!("Reached breakpoint at {}s\n", secs)
+    }
+    
 }
